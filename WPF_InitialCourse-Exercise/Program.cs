c5cad1a1e5c9f9d2b1c3744eb95290239d7d6b7b
André Zottis <zottis.andre@outlook.com>
2019-01-22 21:53:12 -0200

    Adicionar arquivos de projeto.

---
+++ WPF_InitialCourse-Exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_InitialCourse_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type the number to the exercise: \n" +
+                "1. Count divisibles \n" +
+                "2. Infinite SUM \n" +
+                "3. Factoration");
+            int value = int.Parse(Console.ReadLine());
+            switch (value)
+            {
+                case 1:
+                    Divisible.CheckDivisible();
+                    break;
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ WPF_InitialCourse-Exercise/Sessao 5 aula 49/Classes/Divisible.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_InitialCourse_Exercise
+{
+    class Divisible
+    {
+        public static void CheckDivisible ()
+        {
+            
+            var count = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+               if (i % 3 ==0)
+                {
+                    count++;
+                    Console.WriteLine("the number is: {0}", i);
+                }
+
+            }
+
+            Console.WriteLine("The total of number disivible by 3 are: {0}", count);
+            Console.ReadLine();
+
+        }
+    }
+}
